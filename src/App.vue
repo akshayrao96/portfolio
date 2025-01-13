8775363e087bc241dd1e5676f697e9b33a35b43d
Akshay <akshayrao196@gmail.com>
2025-01-13 00:17:16 -0800

    feat: setup and nav bar

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-[#111827] min-h-screen">
+    <NavBar />
+  </div>
+  <HeroSection />
+</template>
+
+<script setup>
+  import { defineAsyncComponent } from 'vue';
+  // Impor Components Asynchronously
+  const NavBar = defineAsyncComponent(() => import('@/components/NavBar.vue'));
+</script>
