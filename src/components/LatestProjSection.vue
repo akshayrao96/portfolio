a14f758f1905db5ec295bb789049233acc42e060
Akshay <akshayrao196@gmail.com>
2025-01-29 01:48:20 -0800

    chore: refactor styling for projects

--- src/components/LatestProjSection.vue
+++ src/components/LatestProjSection.vue
@@ -3,24 +3,34 @@
     <div class="px-4 xl:pl-16">
       <div class="mb-4 md:flex md:justify-between xl:pr-16">
         <h2 class="text-4xl font-bold text-white">My Latest Projects</h2>
-        <div class="flex space-x-4 mb-4 mt-5 md:mt-0">
+
+        <!-- Simple Clickable Category Filters (No Borders, Clean Look) -->
+        <div class="flex flex-wrap gap-4 mb-4 mt-5 md:mt-0">
           <button
-            class="hover:text-primary"
-            v-for="category in ['all', 'web development', 'Mobile App']"
+            v-for="category in categories"
             :key="category"
-            @click="() => (selectedCategory = category)"
+            @click="selectedCategory = category"
+            class="text-white text-lg transition-all duration-300"
+            :class="{
+              'text-primary font-semibold': selectedCategory === category,
+              'hover:text-primary': selectedCategory !== category,
+            }"
           >
             {{ category }}
           </button>
         </div>
       </div>
+
       <ul
         class="px-4 sm:py-16 xl:pr-16 grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3"
         data-aos="fade-right"
       >
         <div v-for="project in filteredProjects" :key="project.id">
-          <div
-            class="h-52 md:h-[24rem] rounded-t-xl relative group"
+          <a
+            :href="project.gitURL"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="block h-52 md:h-[24rem] rounded-t-xl relative group transition-all duration-300 hover:scale-105"
             :style="{
               backgroundImage: 'url(' + project.image + ')',
               backgroundSize: 'cover',
@@ -29,22 +39,14 @@
             <div
               class="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500"
             >
-              <!-- Left Button: GitHub Source Code -->
-              <a
-                class="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
-                :href="project.gitURL"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+              <div class="h-14 w-14 relative flex items-center justify-center">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke-width="1.5"
                   stroke="currentColor"
-                  aria-hidden="true"
-                  data-slot="icon"
-                  class="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white"
+                  class="h-10 w-10 text-[#ADB7BE] group-hover:text-white transition-all duration-300"
                 >
                   <path
                     stroke-linecap="round"
@@ -55,14 +57,14 @@
                     stroke-linecap="round"
                     stroke-linejoin="round"
                     d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                  ></path></svg
-              ></a>
+                  ></path>
+                </svg>
+              </div>
             </div>
-          </div>
+          </a>
           <div
             class="text-white rounded-b-xl mt-3 bg-[#111a3e] shadow-lg border border-[#1f1641] py-6 px-4"
           >
-            <!-- Centered, Clickable Subheading -->
             <h3 class="text-lg font-semibold uppercase lg:text-xl text-center">
               <a
                 :href="project.gitURL"
@@ -95,51 +97,64 @@
     </div>
   </section>
 </template>
-
 <script setup>
   import { ref, computed } from 'vue';
+
+  const categories = ref([
+    'Distributed Systems',
+    'DevOps & Automation',
+    'Full-Stack Development',
+    'Systems Programming',
+    'Computer Networking',
+  ]);
+
+  const selectedCategory = ref('All');
 
   const Projects = ref([
     {
       id: 1,
-      category: 'DevOps & Automation',
+      categories: ['Systems Programming'],
       image: 'src/assets/projects/id1.png',
       title: 'CI Engine',
       description:
-        'A lightweight CI/CD pipeline service for seamless developer workflow integration. Automate builds, tests, and deployments with CLI tools and Docker support.',
+        'A robust CI/CD service designed to streamline developer workflow integration. This system automates build, test, and deployment pipelines, ensuring seamless code delivery.',
       technologies: ['Python', 'CLI Tools', 'Docker', 'Git', 'MongoDB'],
       gitURL: 'https://github.com/akshayrao96/ci-engine',
     },
     {
       id: 2,
-      category: 'Full Stack Development',
+      categories: ['Full-Stack Development'],
       image: 'src/assets/projects/id2.jpg',
       title: 'Password Manager Vault',
       description:
-        'A secure, full-stack password manager with encrypted storage. Built with React and Node.js, it supports user authentication and role-based access. Currently in progress.',
+        'A password manager for storing passwords securely. Includes a full-stack application that manages users, ownership rights, and password encryption.',
       technologies: ['JavaScript', 'NodeJS', 'React', 'HTML', 'CSS', 'MongoDB'],
       gitURL: 'https://github.com/akshayrao96/password-manager-vault',
     },
     {
       id: 3,
-      category: 'Distributed Systems',
+      categories: ['Distributed Systems'],
       image: 'src/assets/projects/id3.jpg',
       title: 'Distributed Album Store',
       description:
-        'A scalable album store built on AWS, integrating dynamic server frameworks, load balancing, and RabbitMQ for high throughput and reliability.',
-      technologies: ['Java', 'Servlets', 'DynamoDB', 'MySQL', 'AWS'],
+        'Client and server system for an optimized distributed architecture. Hosted on AWS, scales via dynamic frameworks, load balancing, and RabbitMQ integration for high throughput.',
+      technologies: ['Java', 'RabbitMQ', 'DynamoDB', 'MySQL', 'AWS'],
       gitURL: 'https://github.com/akshayrao96/distributed-album-store',
     },
     {
       id: 4,
-      category: 'Microservices',
+      categories: [
+        'Microservices',
+        'Distributed Systems',
+        'Full-Stack Development',
+      ],
       image: 'src/assets/projects/id4.jpg',
       title: 'Katallage',
       description:
-        'A dynamic e-commerce platform enabling users to buy and offer freelance services. Built with microservices architecture and Kubernetes deployment. Currently in progress.',
+        'An e-commerce platform where users can buy or sell services. Uses event-based microservices architecture using Kubernetes as orchestration management, RabbitMQ, and observability tools.',
       technologies: [
         'TypeScript',
-        'RabbitMQ',
+        'Redis',
         'Kubernetes',
         'Grafana',
         'Prometheus',
@@ -148,34 +163,30 @@
     },
     {
       id: 5,
-      category: 'System Programming',
+      categories: ['Systems Programming'],
       image: 'src/assets/projects/id5.png',
       title: 'Task Scheduler',
       description:
-        'A Linux kernel-inspired task scheduler simulator, implementing priority-based scheduling and resource allocation for process management. Analyzes metrics and tradeoffs for different scheduling algorithms.',
+        'A simulation of a Linux-based task scheduler, implementing multiple scheduling algorithms and resource allocation methods. Analyzes metrics and tradeoffs between algorithms.',
       technologies: ['C++'],
       gitURL: 'https://github.com/yourrepo/task-scheduler',
     },
     {
       id: 6,
-      category: 'Networking & Communication',
+      categories: ['Computer Networking'],
       image: 'src/assets/projects/id6.jpg',
       title: 'Chat Program',
       description:
-        'A Python-based chat application to reinforce networking concepts like TCP/IP, multithreading, and web sockets. Includes a GUI for enhanced usability.',
+        'A Python-based chat application for learning networking fundamentals (TCP/IP, multithreading, web sockets). Includes a GUI for an enhanced user experience.',
       technologies: ['Python', 'TCP/IP', 'Multithreading'],
       gitURL: 'https://github.com/akshayrao96/chat-program',
     },
   ]);
 
-  const selectedCategory = ref('all');
   const filteredProjects = computed(() => {
-    if (selectedCategory.value === 'all') {
-      return Projects.value;
-    }
-    return Projects.value.filter(
-      project =>
-        project.category.toLowerCase() === selectedCategory.value.toLowerCase()
+    if (selectedCategory.value === 'All') return Projects.value;
+    return Projects.value.filter(project =>
+      project.categories.includes(selectedCategory.value)
     );
   });
 </script>
